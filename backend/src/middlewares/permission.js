79e909cba903a0f1f999ed2369293720fcda44f7
new-psam <marcelino.patricio@hotmail.com>
2026-05-07 08:59:59 -0300

    fix: concede passe livre universal para a role admin no middleware

--- backend/src/middlewares/permission.js
+++ backend/src/middlewares/permission.js
@@ -1,7 +1,7 @@
 module.exports = (requiredRole) => {
     return (req, res, next) => {
         // Se a role do usuário não for a necessária (ex: 'professor)
-        if (req.userRole !== requiredRole){
+        if (req.userRole !== requiredRole && req.userRole !== 'admin'){
             return res.status(403).json({ error: 'Acesso negado: Requer permissão de professor.'});
         }
         next();
