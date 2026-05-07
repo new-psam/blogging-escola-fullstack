cbb23dba3539f72a707a09a2f265eb10559e1c03
new-psam <marcelino.patricio@hotmail.com>
2026-05-07 18:47:03 -0300

    fix: ajusta caminho do json para o render

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,8 +1,20 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
 
-// No Render, vamos configurar para ler o arquivo secreto que você vai subir lá
-// Localmente, você aponta para o caminho do arquivo no seu computador
-const serviceAccount = require('./firebase-service-account.json');
+let serviceAccount;
+
+// O Render salva os "Secret Files" sempre nesta pasta especial:
+const renderSecretPath = '/etc/secrets/firebase-service-account.json';
+
+// O nosso caminho local no seu computador:
+const localSecretPath = './firebase-service-account.json';
+
+// O código decide qual usar dependendo de onde está rodando
+if (fs.existsSync(renderSecretPath)) {
+    serviceAccount = require(renderSecretPath);
+} else {
+    serviceAccount = require(localSecretPath);
+}
 
 if (!admin.apps.length) {
     admin.initializeApp({
