93f29c5b4421b2633180542623d5965f1d0e8701
new-psam <marcelino.patricio@hotmail.com>
2026-05-08 16:11:35 -0300

    fix: middleware de auth agora busca o role no mongo

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,41 +1,45 @@
 //const jwt = require('jsonwebtoken');
 const admin = require('../config/firebase')
+const User = require('../models/User')
 
 module.exports = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader) return res.status(401).send({ error: 'Token não informado'});
+    if (!authHeader) return res.status(401).json({ error: 'Token não informado'});
 
     
     
     const parts = authHeader.split(' ');
-    if (parts.length !== 2) return res.status(401).send({error: 'Erro no token'});
+    if (parts.length !== 2) return res.status(401).json({error: 'token mal formatado'});
     
     // o token vem no formato "Bearer ejks.."
-    const [, token] = parts;
+    const [scheme, token] = parts;
 
+    if (!/^Bearer$/i.test(scheme)) {
+        return res.status(401).json({ error: 'Token com formato inválido' });
+    }
+    
     try {
-        // o firebase verifica se o token é válido, se não expiroi,etc
+        // 1. Verifica o token no Firebase (Crachá de Identidade)
         const decodedToken = await admin.auth().verifyIdToken(token);
 
-        // pegamos o UID do Firebase e colocamos na requisição para os controllers usarem
-        req.userFirebaseUid = decodedToken.uid;
-        // Opcional: Se você quiser passar o email também
-        req.userEmail = decodedToken.email;
+        // 2. Busca o usuário no MongoDB usando o UID do Firebase
+        const user = await User.findOne({firebaseUid: decodedToken.uid});
+
+        if (!user) {
+            return res.status(401).json({error: "Usuário não encontrado no banco de dados!"});
+        }
+
+        // 3. Pendura os dados na requisição para os próximos passos usarem
+        req.userFirebaseUid = decodedToken.uid; 
+        req.userId = user._id;     // Essencial para o PostController saber quem é o autor do Post!
+        req.userRole = user.role;  // Essencial para o permission.js saber se é Professor/Admin!
+
 
         return next();
     } catch (err) {
         console.error("Erro n averificação do token:", err)
         return res.status(401).json({ error: 'token inválido ou expirado.'});
     }
-
-    // const [scheme, token] = parts;
-    // if (!/^Bearer$/i.test(scheme)) return res.status(401).send({error: 'Token malformatado'});
-
-    // jwt.verify(token, process.env.SECRET || 'segredo_escola', (err, decoded) => {
-    //     if (err) return res.status(401).send({error: 'Token inválido' });
-    //     req.userId = decoded.id;
-    //     req.userRole = decoded.role; // Salvamos a role para checar depois
-    //     return next();
-    // });
+    
 };
